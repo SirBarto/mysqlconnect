88d25ebe81ba00429262fd51e73fd57aca5c785d
Bartosz Gawron <bartho58@gmail.com>
2018-12-23 01:19:45 +0100

    Add "Beer" table with configuration

--- src/main/java/com/example/demo/Service/BeerService.java
+++ src/main/java/com/example/demo/Service/BeerService.java
@@ -1,4 +1,45 @@
 package com.example.demo.Service;
 
-public class BeerService {
+import com.example.demo.model.Beer;
+import com.example.demo.repository.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BeerService implements IBeerService {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Override
+    public Beer getBeerById(int id) {
+        Beer obj = beerRepository.findById(id).get();
+        return obj;
+    }
+
+    @Override
+    public void addNewBeer(Beer beer) {
+        beerRepository.save(beer);
+    }
+
+    @Override
+    public void updateBeer(Beer beer) {
+        beerRepository.save(beer);
+    }
+
+    @Override
+    public void deleteBeer(int id) {
+        beerRepository.delete(getBeerById(id));
+    }
+
+    @Override
+    public List<Beer> getAllBeers() {
+        List<Beer> list = new ArrayList<>();
+        beerRepository.findAll().forEach(e -> list.add(e));
+        return list;
+    }
+
 }
--- src/main/java/com/example/demo/controller/BeerController.java
+++ src/main/java/com/example/demo/controller/BeerController.java
@@ -1,4 +1,64 @@
 package com.example.demo.controller;
 
+import com.example.demo.Service.BeerService;
+import com.example.demo.model.Beer;
+import com.example.demo.repository.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path = "/demo")
 public class BeerController {
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    BeerService beerService;
+
+    @PostMapping(path = "/beer/add")
+    public ResponseEntity<Void> addNewUser(@RequestBody Beer beer) {
+        Beer savedBeer = beerRepository.save(beer);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedBeer.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping(path = "/beer/{id}")
+    public ResponseEntity<Beer> getBeer(@PathVariable("id") int id) {
+        Beer n = beerService.getBeerById(id);
+        return new ResponseEntity<Beer>(n, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/beer/all")
+    public ResponseEntity<List<Beer>> getAllBeers() {
+        List<Beer> list = beerService.getAllBeers();
+        return new ResponseEntity<List<Beer>>(list, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/beer/remove/{id}")
+    public ResponseEntity<Void> deleteBeer(@PathVariable("id") int id) {
+        beerService.deleteBeer(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping(path = "/beer/update/{id}")
+    public ResponseEntity<Beer> updateBeer(@RequestBody Beer beer, @PathVariable int id) {
+        Optional<Beer> beerOptional = beerRepository.findById(id);
+        if (!beerOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        beer.setId(id);
+        beerRepository.save(beer);
+        return ResponseEntity.noContent().build();
+    }
+
 }
